b69a12ceeda80b196a0d58597fce8d13a11f0072
phamthanhson0111@gmail.com <phamthanhson0111@gmail.com>
2022-11-18 22:50:41 +0700

    deploy user page

--- admin/src/components/LoadingError/ToastObject.js
+++
@@ -1,6 +0,0 @@
-export const ToastObject = {
-  pauseOnFocusLoss: false,
-  draggable: false,
-  pauseOnHover: false,
-  autoClose: 2000,
-};
--- admin/src/utils/request.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-const request = axios.create({
-    baseURL: 'http://localhost:5000/',
-});
-
-export default request;
